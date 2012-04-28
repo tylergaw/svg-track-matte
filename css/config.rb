6ede7210f72336f7561fe3697c21c0e6c671dafb
Tyler Gaw <me@tylergaw.com>
2012-04-28 13:46:20 -0400

    initial commit

---
+++ css/config.rb
@@ -0,0 +1,17 @@
+http_path = "/"
+
+css_dir = "."
+sass_dir = "scss"
+
+# Images
+images_dir = "../images"
+
+# Output
+output_style = :expanded
+line_comments = false
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+preferred_syntax = :scss
+
+asset_cache_buster :none
